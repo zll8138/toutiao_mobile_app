fda5fc07adc4f3a2de56d2ec11a7caa2b3fb68fe
zll <3014576455@qq.com>
2020-11-27 19:43:45 +0800

    login style

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <h1>昨日头条</h1>
     <router-view/>
   </div>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="home-container">登录</div>
+  <div class="login-container">
+    <van-nav-bar
+      title="注册 / 登录"
+      left-arrow
+      class="app-nav-bar"
+      @click-left="$router.back()"
+    />
+
+    <!-- 登录表单 -->
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        icon-prefix="toutiao"
+        left-icon="shouji"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        icon-prefix="toutiao"
+        left-icon="yanzhengma"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            class="send-btn"
+            size="mini"
+            round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn-wrap">
+      <van-button class="login-btn" type="info" block>登录</van-button>
+    </div>
+    <!-- /登录表单 -->
+  </div>
 </template>
 
 <script>
@@ -8,7 +44,12 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
   },
   computed: {},
   watch: {},
@@ -18,4 +59,26 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
+</style>
