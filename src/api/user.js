29ff2326279cad7bb60f74d7a198fa4c23b3343d
zll <3014576455@qq.com>
2020-11-30 10:41:13 +0800

    token interceptor

--- src/api/user.js
+++ src/api/user.js
@@ -2,9 +2,7 @@
  * 用户相关请求模块
  */
 import request from '@/utils/request'
-// 在非组件模块中获取 store 必须通过这种方式
-// 这里单独加载 store，和在组件中 this.$store 一个东西
-import store from '@/store/'
+
 /**
  * 登录/注册
  */
@@ -22,9 +20,6 @@ export const login = data => {
 export const getCurrentUser = () => {
   return request({
     method: 'GET',
-    url: '/app/v1_0/user',
-    headers: {
-      Authorization: `Bearer ${store.state.user.token}`
-    }
+    url: '/app/v1_0/user'
   })
 }
