c359f3e9d735d03fc4e227309cf2faf8f866eb64
zll <3014576455@qq.com>
2020-11-30 15:06:12 +0800

    search func

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ const routes = [
         path: '/my',
         name: 'my',
         component: () => import('@/views/my/')
+      },
+      {
+        path: '/search',
+        name: 'search',
+        component: () => import('@/views/search/')
       }
     ]
   }
