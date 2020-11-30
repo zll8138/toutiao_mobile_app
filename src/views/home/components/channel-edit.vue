e6e0caa386f4c8d676969217f747aa6a7ac02784
zll <3014576455@qq.com>
2020-11-30 14:16:02 +0800

    edit channel4

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -7,7 +7,8 @@
         plain
         round
         size="mini"
-      >编辑</van-button>
+        @click="isEdit = !isEdit"
+      >{{ isEdit ? '完成' : '编辑' }}</van-button>
     </van-cell>
 
     <van-grid :gutter="10">
@@ -16,6 +17,8 @@
         v-for="(channel, index) in userChannels"
         :key="index"
         :text="channel.name"
+        :icon="(isEdit && index !== 0) ? 'clear' : ''"
+        @click="onUserChannelClick(index)"
       />
     </van-grid>
 
@@ -29,6 +32,7 @@
         v-for="(channel, index) in recommendChannels"
         :key="index"
         :text="channel.name"
+        @click="onAdd(channel)"
       />
     </van-grid>
   </div>
@@ -47,7 +51,8 @@ export default {
   },
   data () {
     return {
-      allChannels: []
+      allChannels: [],
+      isEdit: false // 控制编辑的显示状态
     }
   },
   computed: {
@@ -71,6 +76,26 @@ export default {
   },
   mounted () {},
   methods: {
+    onUserChannelClick (index) {
+      if (this.isEdit && index !== 0) {
+        // 编辑状态，删除频道
+        this.deleteChannel(index)
+      } else {
+        // 非编辑状态，切换频道
+        this.switchChannel(index)
+      }
+    },
+    deleteChannel (index) {
+      this.userChannels.splice(index, 1)
+      // 数据持久化
+    },
+    switchChannel (index) {
+      console.log('切换频道')
+    },
+    onAdd (channel) {
+      this.userChannels.push(channel)
+      // TODO: 数据持久化
+    },
     async loadAllChannels () {
       const { data } = await getAllChannels()
       this.allChannels = data.data.channels
@@ -80,22 +105,42 @@ export default {
 </script>
 
 <style scoped lang="less">
-.channel-edit {
-  padding-top: 54px;
-  .channel-title {
+.article-item {
+  .title {
     font-size: 16px;
-    color: #333333;
+    color: #3a3a3a;
   }
-  .grid-item {
-    width: 80px;
-    height: 43px;
-    /deep/ .van-grid-item__content {
-      background-color: #f4f5f6;
-      .van-grid-item__text {
-        font-size: 14px;
-        color: #222;
+  /deep/ .van-cell__value {
+    flex: unset;
+    width: 116px;
+    height: 73px;
+    margin-left: 12px;
+  }
+  .right-cover {
+    width: 116px;
+    height: 73px;
+  }
+  .cover-wrap {
+    padding: 15px 0;
+    display: flex;
+    .cover-wrap-item {
+      flex: 1;
+      height: 73px;
+      &:not(:last-child) {
+        padding-right: 4px;
+      }
+      .cover-image {
+        width: 100%;
+        height: 73px;
       }
     }
+  }
+  .label-wrap {
+    font-size: 11px;
+    color: #b4b4b4;
+  }
+  .label-wrap span {
+    margin-right: 12px;
   }
 }
 </style>
