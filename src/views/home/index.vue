6de36df8721d521473149874cfc0b3613375d097
zll <3014576455@qq.com>
2020-11-30 10:44:43 +0800

    home nav and tab

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="app-nav-bar">
+      <van-button
+        slot="title"
+        class="search-btn"
+        icon="search"
+        type="info"
+        round
+        size="small"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+    <!-- 文章频道列表 -->
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
@@ -8,7 +29,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      active: 1 // 控制被激活的标签
+    }
   },
   computed: {},
   watch: {},
@@ -18,4 +41,22 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  /deep/ .van-nav-bar__title {
+    max-width: none;
+  }
+  .search-btn {
+    width: 277px;
+    height: 32px;
+    background: #5babfb;
+    border: none;
+    .van-icon {
+      font-size: 16px;
+    }
+    .van-button__text {
+      font-size: 14px;
+    }
+  }
+}
+</style>
