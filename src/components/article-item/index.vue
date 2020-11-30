e2d355f9a381381c9e8fcc2ebd59dfb4f7c8fecb
zll <3014576455@qq.com>
2020-11-30 11:34:02 +0800

    add article-item component

---
+++ src/components/article-item/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <van-cell class="article-item">
+    <div slot="title">{{ article.title }}</div>
+    <div slot="label">
+      <div
+        v-if="article.cover.type === 3"
+        class="cover-wrap"
+      >
+        <div class="cover-wrap-item">
+          <van-image
+            width="116"
+            height="73"
+            fit="cover"
+            v-for="(img, index) in article.cover.images"
+            :key="index"
+            :src="img"
+          />
+        </div>
+      </div>
+      <div class="label-wrap">
+        <span>{{ article.aut_name }}</span>
+        <span>{{ article.comm_count }}评论</span>
+        <span>{{ article.pubdate }}</span>
+      </div>
+    </div>
+    <!-- 默认插槽的名字叫 default，可以省略 -->
+    <van-image
+      v-if="article.cover.type === 1"
+      width="116"
+      height="73"
+      fit="cover"
+      :src="article.cover.images[0]"
+    />
+  </van-cell>
+</template>
+
+<script>
+export default {
+  name: 'ArticleItem',
+  components: {},
+  props: {
+    article: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -26,10 +26,10 @@
         finished-text="没有更多了"
         @load="onLoad"
       >
-        <van-cell
+        <article-item
           v-for="(article, index) in articles"
           :key="index"
-          :title="article.title"
+          :article="article"
         />
       </van-list>
     </van-pull-refresh>
@@ -38,9 +38,12 @@
 
 <script>
 import { getArticles } from '@/api/article'
+import ArticleItem from '@/components/article-item'
 export default {
   name: 'ArticleList',
-  components: {},
+  components: {
+    ArticleItem
+  },
   props: {
     channel: {
       type: Object,
