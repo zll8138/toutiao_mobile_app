e3dce306b861d196c66759715ffc2f4bc453303b
zll <3014576455@qq.com>
2020-12-01 10:36:01 +0800

    user edit

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,17 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+// 路由表
 const routes = [
   {
     path: '/login',
     name: 'login',
     component: () => import('@/views/login/')
   },
-  {
+  { // 一级路由渲染到根组件中的 router-view
     path: '/',
     component: () => import('@/views/layout/'),
-    children: [
+    children: [ // 子路由渲染到父路由的 router-view 中
       {
         path: '', // 默认子路由
         name: 'home',
@@ -32,20 +33,26 @@ const routes = [
         path: '/my',
         name: 'my',
         component: () => import('@/views/my/')
-      },
-      {
-        path: '/search',
-        name: 'search',
-        component: () => import('@/views/search/')
-      },
-      {
-        path: '/article/:articleId',
-        name: 'article',
-        component: () => import('@/views/article/'),
-        // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
-        props: true
       }
     ]
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('@/views/search/')
+  },
+  {
+    path: '/article/:articleId',
+    name: 'article',
+    component: () => import('@/views/article/'),
+    // 将动态路由参数映射到组件的 props 中，无论是访问还是维护性都很方便
+    // 参考文档：https://router.vuejs.org/zh/guide/essentials/passing-props.html
+    props: true
+  },
+  {
+    path: '/user/profile',
+    name: 'user-profile',
+    component: () => import('@/views/user-profile')
   }
 ]
 
