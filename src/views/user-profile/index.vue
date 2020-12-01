b2459f84321cab6717fa04ca9107f07a0f142efe
zll <3014576455@qq.com>
2020-12-01 10:53:15 +0800

    user edit4

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -27,11 +27,13 @@
       title="性别"
       is-link
       :value="user.gender === 0 ? '男' : '女'"
+      @click="isEditGenderShow = true"
     />
     <van-cell
       title="生日"
       is-link
       :value="user.birthday"
+      @click="isEditBirthdayShow = true"
     />
 
     <van-popup
@@ -45,22 +47,53 @@
         @update-name="user.name = $event"
       />
     </van-popup>
+
+    <!-- 修改性别 -->
+    <van-popup
+      v-model="isEditGenderShow"
+      position="bottom"
+    >
+      <update-gender
+        v-model="user.gender"
+        @close="isEditGenderShow = false"
+      />
+    </van-popup>
+    <!-- /修改性别 -->
+
+    <!-- 修改生日 -->
+    <van-popup
+      v-model="isEditBirthdayShow"
+      position="bottom"
+    >
+      <update-birthday
+        v-if="isEditBirthdayShow"
+        v-model="user.birthday"
+        @close="isEditBirthdayShow = false"
+      />
+    </van-popup>
+    <!-- /修改生日 -->
   </div>
 </template>
 
 <script>
 import { getUserProfile } from '@/api/user'
 import UpdateName from './components/update-name'
+import UpdateGender from './components/update-gender'
+import UpdateBirthday from './components/update-birthday'
 export default {
   name: 'UserProfile',
   components: {
-    UpdateName
+    UpdateName,
+    UpdateGender,
+    UpdateBirthday
   },
   props: {},
   data () {
     return {
       user: {}, // 用户数据
-      isEditNameShow: false // 编辑昵称的显示状态
+      isEditNameShow: false, // 编辑昵称的显示状态
+      isEditGenderShow: false, // 编辑性别的显示状态
+      isEditBirthdayShow: false // 编辑生日的显示状态
     }
   },
   computed: {},
