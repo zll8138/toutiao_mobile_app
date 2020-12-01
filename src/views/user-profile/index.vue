0375b723eb5856eae4cbf5137f3089074dac7df3
zll <3014576455@qq.com>
2020-12-01 10:39:21 +0800

    user edit2

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,20 +1,63 @@
 <template>
-  <div class="user-profile">用户资料</div>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <van-cell title="头像" is-link center>
+      <van-image
+        width="30"
+        height="30"
+        round
+        fit="cover"
+        :src="user.photo"
+      />
+    </van-cell>
+    <van-cell
+      title="昵称"
+      is-link
+      :value="user.name"
+    />
+    <van-cell
+      title="性别"
+      is-link
+      :value="user.gender === 0 ? '男' : '女'"
+    />
+    <van-cell
+      title="生日"
+      is-link
+      :value="user.birthday"
+    />
+  </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
 export default {
   name: 'UserProfile',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {} // 用户数据
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadUserProfile()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadUserProfile () {
+      const { data } = await getUserProfile()
+      this.user = data.data
+    }
+  }
 }
 </script>
 
