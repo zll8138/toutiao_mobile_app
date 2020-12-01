48e7ef338a3fb3a739b0e9d701bab7ce023a2fbb
zll <3014576455@qq.com>
2020-12-01 10:28:16 +0800

    comment func7

--- src/views/article/components/comment-list.vue
+++ src/views/article/components/comment-list.vue
@@ -29,6 +29,12 @@ export default {
       type: [Number, String, Object],
       required: true
     },
+    // 获取文章评论，使用字符 a
+    // 获取评论回复，使用字符 c
+    type: {
+      type: String,
+      default: 'a'
+    },
     list: {
       type: Array,
       // 数组或对象的默认值必须通过函数返回
@@ -55,8 +61,8 @@ export default {
     async onLoad () {
       // 1. 请求获取数据
       const { data } = await getComments({
-        type: 'a', // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
-        source: this.source, // 源id，文章id或评论id
+        type: this.type, // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
+        source: this.source.toString(), // 源id，文章id或评论id
         offset: this.offset, // 获取评论数据的偏移量，值为评论id，表示从此id的数据向后取，不传表示从第一页开始读取数据
         limit: this.limit // 每页大小
       })
