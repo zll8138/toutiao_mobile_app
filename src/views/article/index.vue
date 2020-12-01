fb69fc2e14e569151f697d6b870aeebe46c5001c
zll <3014576455@qq.com>
2020-12-01 09:13:59 +0800

    comment func1

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="article-container">
+  <div class="article-container">
     <!-- 导航栏 -->
     <van-nav-bar
       class="app-nav-bar"
@@ -9,43 +9,88 @@
     />
     <!-- /导航栏 -->
 
-    <h1 class="title">{{ article.title }}</h1>
-    <van-cell center class="user-info">
-      <div slot="title" class="name">{{ article.aut_name }}</div>
-      <van-image
-        slot="icon"
-        class="avatar"
-        round
-        fit="cover"
-        :src="article.aut_photo"
-      />
-      <div slot="label" class="pubdate">{{ article.pubdate | relativeTime }}</div>
+    <div class="article-wrap">
+      <h1 class="title">{{ article.title }}</h1>
+      <van-cell center class="user-info">
+        <div slot="title" class="name">{{ article.aut_name }}</div>
+        <van-image
+          slot="icon"
+          class="avatar"
+          round
+          fit="cover"
+          :src="article.aut_photo"
+        />
+        <div slot="label" class="pubdate">{{ article.pubdate | relativeTime }}</div>
+        <van-button
+          class="follow-btn"
+          :type="article.is_followed ? 'default' : 'info'"
+          :icon="article.is_followed ? '' : 'plus'"
+          round
+          size="small"
+          :loading="isFollowLoading"
+          @click="onFollow"
+        >{{ article.is_followed ? '已关注' : '关注' }}</van-button>
+      </van-cell>
+      <div
+        class="markdown-body"
+        v-html="article.content"
+        ref="article-content"
+      ></div>
+      <!-- 文章评论列表 -->
+      <comment-list />
+      <!-- /文章评论列表 -->
+    </div>
+
+    <!-- 底部区域 -->
+    <div class="article-bottom">
       <van-button
-        class="follow-btn"
-        :type="article.is_followed ? 'default' : 'info'"
-        :icon="article.is_followed ? '' : 'plus'"
+        class="comment-btn"
+        type="default"
         round
         size="small"
-        :loading="isFollowLoading"
-        @click="onFollow"
-      >{{ article.is_followed ? '已关注' : '关注' }}</van-button>
-    </van-cell>
-    <div class="markdown-body" v-html="article.content" ref="article-content"></div>
+      >写评论</van-button>
+      <van-icon
+        name="comment-o"
+        info="123"
+        color="#777"
+      />
+      <van-icon
+        :color="article.is_collected ? 'orange' : '#777'"
+        :name="article.is_collected ? 'star' : 'star-o'"
+        @click="onCollect"
+      />
+      <van-icon
+        :color="article.attitude === 1 ? 'hotpink' : '#777'"
+        :name="article.attitude === 1 ? 'good-job' : 'good-job-o'"
+        @click="onLike"
+      />
+      <van-icon name="share" color="#777777"></van-icon>
+    </div>
+    <!-- /底部区域 -->
   </div>
 </template>
 
 <script>
 import './github-markdown.css'
-import { getArticleById } from '@/api/article'
+import {
+  getArticleById,
+  addCollect,
+  deleteCollect,
+  addLike,
+  deleteLike
+} from '@/api/article'
 import { ImagePreview } from 'vant'
 import { addFollow, deleteFollow } from '@/api/user'
-// 在组件中获取动态路由参数：
-//    方式一：this.$route.params.articleId
-//    方式二：props 传参，推荐
-//      this.articleId
+import CommentList from './components/comment-list'
 export default {
   name: 'ArticleIndex',
-  components: {},
+  components: {
+    CommentList
+  },
+  // 在组件中获取动态路由参数：
+  //    方式一：this.$route.params.articleId
+  //    方式二：props 传参，推荐
+  //      this.articleId
   props: {
     articleId: {
       type: [String, Number, Object],
@@ -54,9 +99,9 @@ export default {
   },
   data () {
     return {
-      article: {},
-      errStatus: 0, // 失败的状态码
-      isFollowLoading: false // 文章数据对象
+      article: {}, // 文章数据对象
+      isFollowLoading: false, // 关注用户按钮的 loading 状态
+      isCollectLoading: false // 收藏的 loading 状态
     }
   },
   computed: {},
@@ -66,6 +111,35 @@ export default {
   },
   mounted () {},
   methods: {
+    async loadArticle () {
+      const { data } = await getArticleById(this.articleId)
+      this.article = data.data
+      // 数据改变影响视图更新（DOM数据）不是立即的
+      // 所以如果需要在修改数据之后马上操作被该数据影响的视图 DOM，需要把这个代码放到 $nextTick 中
+      // this.$nextTick 是 Vue 提供的一个方法
+      // 参考文档：
+      this.$nextTick(() => {
+        this.handlePerviewImage()
+      })
+    },
+    handlePerviewImage () {
+      // 1. 获取文章内容 DOM 容器
+      const articleContent = this.$refs['article-content']
+      // 2. 得到所有的 img 标签
+      const imgs = articleContent.querySelectorAll('img')
+      const imgPaths = [] // 收集所有的图片路径
+      // 3. 循环 img 列表，给 img 注册点击事件
+      imgs.forEach((img, index) => {
+        imgPaths.push(img.src)
+        img.onclick = function () {
+          // 4. 在事件处理函数中调用 ImagePreview() 预览
+          ImagePreview({
+            images: imgPaths, // 预览图片路径列表
+            startPosition: index // 起始位置
+          })
+        }
+      })
+    },
     async onFollow () {
       this.isFollowLoading = true
       if (this.article.is_followed) {
@@ -80,40 +154,50 @@ export default {
       this.article.is_followed = !this.article.is_followed
       this.isFollowLoading = false
     },
-    previewImage () {
-      // 得到所有的 img 节点
-      const articleContent = this.$refs['article-content']
-      const imgs = articleContent.querySelectorAll('img')
-      // 获取所有 img 地址
-      const images = []
-      imgs.forEach((img, index) => {
-        images.push(img.src)
-        // 给每个 img 注册点击事件，在处理函数中调用预览
-        img.onclick = () => {
-          ImagePreview({
-            // 预览的图片地址数组
-            images,
-            // 起始位置，从 0 开始
-            startPosition: index
-          })
-        }
+    async onCollect () {
+      this.$toast.loading({
+        message: '操作中...',
+        forbidClick: true // 禁止背景点击
       })
+      if (this.article.is_collected) {
+        // 已收藏，取消收藏
+        await deleteCollect(this.articleId)
+      } else {
+        // 没有收藏，添加收藏
+        await addCollect(this.articleId)
+      }
+      this.article.is_collected = !this.article.is_collected
+      this.$toast.success(`${this.article.is_collected ? '' : '取消'}收藏成功`)
     },
-    async loadArticle () {
-      const { data } = await getArticleById(this.articleId)
-      // 数据驱动视图这件事儿不是立即的
-      this.article = data.data
-      // 初始化图片点击预览
-      // console.log(this.$refs['article-content'])
-      setTimeout(() => {
-        this.previewImage()
-      }, 0)
+    async onLike () {
+      this.$toast.loading({
+        message: '操作中...',
+        forbidClick: true // 禁止背景点击
+      })
+      if (this.article.attitude === 1) {
+        // 已点赞，取消点赞
+        await deleteLike(this.articleId)
+        this.article.attitude = -1
+      } else {
+        // 没有点赞，添加点赞
+        await addLike(this.articleId)
+        this.article.attitude = 1
+      }
+      this.$toast.success(`${this.article.attitude === 1 ? '' : '取消'}点赞成功`)
     }
   }
 }
 </script>
 
 <style scoped lang="less">
+.article-wrap {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 46px;
+  bottom: 44px;
+  overflow-y: auto;
+}
 .title {
   font-size: 20px;
   color: #3a3a3a;
@@ -146,5 +230,33 @@ ul {
 .markdown-body {
   padding: 14px;
   background-color: #fff;
+}
+.article-bottom {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-sizing: border-box;
+  height: 44px;
+  border-top: 1px solid #d8d8d8;
+  background-color: #fff;
+  .comment-btn {
+    width: 141px;
+    height: 23px;
+    border: 1px solid #eeeeee;
+    font-size: 15px;
+    line-height: 23px;
+    color: #a7a7a7;
+  }
+  .van-icon {
+    font-size: 20px;
+    .van-info {
+      font-size: 11px;
+      background-color: #e22829;
+    }
+  }
 }
 </style>
